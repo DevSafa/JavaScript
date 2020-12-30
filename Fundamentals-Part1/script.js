4cc191a97e0e15afc6b15d113a02cf813f5a25d9
Safa Barka <sbarka@e1r12p8.1337.ma>
2020-12-30 16:05:43 +0100

    Values and Variables+...

--- Fundamentals-Part1/script.js
+++ Fundamentals-Part1/script.js
@@ -1,6 +1,49 @@
 
-let js = 'amazing';
-if (js === 'amazing') alert('JAVASCRIPT IS FUN!');
-//tell JavaScript to explicitly output(print) the result of (40 + 8 + 23 - 10) to the console,
-//using special function console.log().
+
+//print a the value 'Jonas' in  the console
+console.log("Jonas");
+console.log(23);
 console.log(40 + 8 + 23 - 10);
+
+//this will create a real variable in your computer memory  and store this value 'safa' inside of that variable 
+let firstName = "Inas";
+
+//declare a variable called js and assign to it the value "amazing"
+let js = "amazing";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+firstName = "Safa";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let safa$inas = "SI";
+console.log(safa$inas);
+
+let safa_inas = "S_I";
+console.log(safa_inas);
+
+let $safa$inas$ = "safainass";
+console.log($safa$inas$);
+
+let PI = 3.1415;
+
+//the variable names myFirstJob , myCurrentJob are more descriptive
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+//the variables names job1 , job2 are less descriptive 
+let job1 = "programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+//error
+    //let 3years = 37; 
+    //let jonas&matilda = 'JM'; 
+    //let new 27; mew is a reserevd keyword in js 
+    
+
