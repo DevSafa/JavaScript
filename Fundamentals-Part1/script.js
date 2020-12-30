176e2af92816a682a6689e793fb162bde7fa491d
Safa Barka <sbarka@e1r12p8.1337.ma>
2020-12-30 15:19:33 +0100

    Linking a JavaScript file to an HTML page + disadvantage and advantage  of inline script +...

---
+++ Fundamentals-Part1/script.js
@@ -0,0 +1,6 @@
+
+let js = 'amazing';
+if (js === 'amazing') alert('JAVASCRIPT IS FUN!');
+//tell JavaScript to explicitly output(print) the result of (40 + 8 + 23 - 10) to the console,
+//using special function console.log().
+console.log(40 + 8 + 23 - 10);
