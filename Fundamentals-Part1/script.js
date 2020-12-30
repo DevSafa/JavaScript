c5eff1a87f8c012bd47ed4a9b95bff230824a3b7
Safa Barka <sbarka@e1r12p8.1337.ma>
2020-12-30 16:58:00 +0100

    js Data Types + typeof operator + dynamic typing +...

--- Fundamentals-Part1/script.js
+++ Fundamentals-Part1/script.js
@@ -1,5 +1,5 @@
 
-
+/*
 //print a the value 'Jonas' in  the console
 console.log("Jonas");
 console.log(23);
@@ -46,4 +46,46 @@ console.log(myFirstJob);
     //let jonas&matilda = 'JM'; 
     //let new 27; mew is a reserevd keyword in js 
     
+*/
+
+// true is a boolean value
+console.log(true);
+
+//declaring a variable name , and assign to it a boolean value
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+
+//using typeof operator to get the type of value 
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'INAS');
+console.log(typeof "INAS");
+
+//dynamic typing means that we can simply change the type of a value that is hold by a variable
+
+javaScriptIsFun = 'NO!';
+//change the data type from boolean to string
+console.log(typeof javaScriptIsFun);
+
+let year;
+//whenever we declared an empty variables , the value of the variable will be undefined ,
+//and the type of teh variable is also undefined.
+//we should see undefined
+console.log(year);
+console.log(typeof year);
+
+//reassign the variable year that already hold undefined value.
+year = 8787;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof undefined);
+// the js says that type of null is object , and this doesn't make any sense at all 
+// this is regarded as bug or error in js .
+//the expected output is null 
+// this bug is never correted because of legacy reasons ,(null is not an object).
+console.log(typeof null);
+
 
