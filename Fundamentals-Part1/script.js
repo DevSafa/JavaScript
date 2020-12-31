e6c20b973e56f49c4c02a8d5d4366e2b2670e17f
sbarka-safa <sbarka@e1r12p8.1337.ma>
2020-12-31 11:26:30 +0100

    Basic operators+...

--- Fundamentals-Part1/script.js
+++ Fundamentals-Part1/script.js
@@ -1,34 +1,51 @@
-//we use the let keyword to declare variables that can change later,
-//during the execution of our program .
 
 
-let age = 30 ;
-console.log(age);
-//in  technical terms we call this reassigning a value to a variable .or also we say that we mutate the age variable .
-age = 31;
-console.log(age);
 
-//the value in a const variable cannot be changed
-//const keyword creates a variable that we cannot reassign to it .
-//birthyEAR IS AN immutable variable.
-const birthYear = 1991;
-//type error
-    // birthYear = 1990;
+const now = 2021;
+//---------------
+//Math operators
+//----------------
 
-// we cannot declare empty const variables
-    //const job;
+//use the minus operator to calculate an age
+const ageJonas = now - 1990;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
 
-// var is basically the old way for defining variables
-//at first side it works actually pretty much as let.
-// but of course there is difference between let and const.
-//yous hould basically never use var 
-var job = "programmer";
-console.log(job);
+console.log(ageJonas * 2, ageJonas / 10 , 2**3 );
+//2**3 means 2 to the power of 3 = 2 * 2 * 2 
 
-job = "nothing";
-console.log(job);
 
-// declaring a variable without any keyword (let , const , var) still works
-// is a very terrible idea  because it doesn't create a variable in the current scope.
-lastName = 'abau';
-console.log(lastName);
+const firstName = "Safa";
+const lastName = "Barka";
+//use the + operator for concatenation
+console.log(firstName + lastName);
+console.log(firstName + " " +lastName);
+
+//---------------------
+//assignement operator
+//---------------------
+
+//the + operator is executed before the assignement operator
+let x = 10 + 5;
+console.log(x);
+x+=10; // x = x + 10 = 15 + 10 = 25
+x *= 4 ; // x = x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x= x -1 = 100
+x--; // 99
+console.log(x);
+
+//----------------------
+//comparison operators
+//----------------------
+
+//we use comparison operators to produce boolean values : >, <, >= , <=
+console.log(ageJonas > ageSarah); // true 31 > 3 
+console.log(ageSarah >= 18); // false
+
+//is FullAge will be the variable holding the boolean value
+const isFullAge = ageSarah >= 18 
+console.log(isFullAge);
+
+// js will do arithmetic first (now - 1991)  , (now -2018) , and then do comparison (>)
+console.log(now - 1991 > now - 2018);
