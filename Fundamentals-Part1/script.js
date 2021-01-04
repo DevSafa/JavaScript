f59a1a2027f3ff7b5f548c726685f09681690c8d
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-04 12:38:01 +0100

    Strings and Template Literals+...

--- Fundamentals-Part1/script.js
+++ Fundamentals-Part1/script.js
@@ -1,47 +1,33 @@
-////////////
-// Coding Challenge #1
+const firstName ="Inas"
+const job = "Doctor"
+const birthYear = 1991;
+const year = 2037;
+//let's use this variables to build a string 
 
-/*
-    Mark and John are trying to compare their BMI (Body Mass Index),
-    which is calculated using the formula :
-    BMI = mass / height ** 2 = mass /(height * height).
-    (mass in Kg and height in meter).
+//js will convert a number to a string so then it can concatenate them , join them with the rest of the strings
+const inas = "I'm " + firstName + ', a ' + (year - birthYear) 
+            + ' years old ' + job + ' !';
+// it's a pain to manage all this spaces using the concatenation operator (+)
+console.log(inas);
 
-    1- store Mark's and John's mass and height in variables
-    2- Calculate both their BMIs using the formula (you can even implement both versions)
-    3- create a boolean variable 'markHeigherBMI' containing information about whether Mark has a 
-    heigher BMI than John .
+//using template literales -> an easy way .
+//insid the curly braces we can write any javaScript expression.
+const inasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job} !`;
+console.log(inasNew);
 
-    Test Data 1 : Marks weights 78 kg and is 1.69 m tall .
-                John weights 92 Kg and is 1.95 m tall .
-    Test Data 2: Marks weights 95 kg and is 1.88 m tall.
-                John wights 85 kg and is 1.76 m tall
+console.log(`Just a regular string...`);
 
+//multilines string before ES6
+console.log('String with \nmultiple \nlines');
 
-*/
+ console.log('String with\n\
+ multiple\n\
+ lines');
 
-// console.log("Test Data 1:");
-// const massMark = 78;
-// const heightMark = 1.69;
-// const massJohn = 92;
-// const heightJohn = 1.95;
-
-// const BMIMark = massMark / heightMark ** 2;
-// const BMIJohn = massJohn / (heightJohn * heightJohn);
-
-// const markHeigherBMI = BMIMark > BMIJohn;
-// console.log("Mark BMI :"+BMIMark +"\n"+ "John BMI :"+BMIJohn+"\n"+"BMI MARK heigher THAN JOHN :"+markHeigherBMI);
-
-
-console.log("Test Data 2:");
-const massMark = 95;
-const heightMark = 1.88;
-const massJohn = 85;
-const heightJohn = 1.76;
-
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-
-const markHeigherBMI = BMIMark > BMIJohn;
-console.log("Mark BMI :"+BMIMark +"\n"+ "John BMI :"+BMIJohn+"\n"+"BMI MARK heigher THAN JOHN :"+markHeigherBMI);
-
+//with template strings tthere is a n easy way to do that
+//we can just hit ENTER.
+//we use back ticks to create multiline HTML elements
+//to insert them into a page dynamically .
+ console.log(`String with
+ multiple
+ lines`);
