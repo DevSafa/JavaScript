6fdd799f325ba5aaec4aceb9359489f824df1f62
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-04 11:07:42 +0100

    Operator Precedence+...

--- Fundamentals-Part1/script.js
+++ Fundamentals-Part1/script.js
@@ -1,51 +1,38 @@
 
 
-
-const now = 2021;
-//---------------
-//Math operators
-//----------------
-
-//use the minus operator to calculate an age
-const ageJonas = now - 1990;
+const now = 2037;
+const ageJonas = now - 1991;
 const ageSarah = now - 2018;
-console.log(ageJonas,ageSarah);
-
-console.log(ageJonas * 2, ageJonas / 10 , 2**3 );
-//2**3 means 2 to the power of 3 = 2 * 2 * 2 
 
 
-const firstName = "Safa";
-const lastName = "Barka";
-//use the + operator for concatenation
-console.log(firstName + lastName);
-console.log(firstName + " " +lastName);
+//usually all the maths operators are executed before the comparison operators.
+//the substraction operator is executed from left to right .
+//the Greater than operator is executed from left to right .
 
-//---------------------
-//assignement operator
-//---------------------
-
-//the + operator is executed before the assignement operator
-let x = 10 + 5;
-console.log(x);
-x+=10; // x = x + 10 = 15 + 10 = 25
-x *= 4 ; // x = x = x * 4 = 100
-x++; // x = x + 1 = 101
-x--; // x= x -1 = 100
-x--; // 99
-console.log(x);
-
-//----------------------
-//comparison operators
-//----------------------
-
-//we use comparison operators to produce boolean values : >, <, >= , <=
-console.log(ageJonas > ageSarah); // true 31 > 3 
-console.log(ageSarah >= 18); // false
-
-//is FullAge will be the variable holding the boolean value
-const isFullAge = ageSarah >= 18 
-console.log(isFullAge);
-
-// js will do arithmetic first (now - 1991)  , (now -2018) , and then do comparison (>)
 console.log(now - 1991 > now - 2018);
+
+//substraction is left to right  execution 
+console.log(25 - 10 - 5);
+
+//assignement is a good example for right to left execution.
+
+let x,y; // x and y are equal to undefined.
+
+//after the minus operation left to right , we end up with :
+//x = y = 10.
+//the assignement operators are executed right to left so :
+// y = 10 annd the x = 10 ;
+
+// if we start the assignement from left to right , the expected result will be x = undefined , y = 10 ;// and that not the case
+x = y = 25 - 10 - 5; 
+console.log(x, y);
+
+// according to precedence table , the devision is happening before substruction.
+//ageSarah will be devided by 2 and then ageJonas is gonna be added
+const averageAgefalse  = ageJonas + ageSarah / 2;
+//the result is not logic , the average is bigger than the ages
+console.log(ageJonas, ageSarah,averageAgefalse); 
+
+const averageAgeTrue = (ageJonas +ageSarah) / 2;
+console.log(ageJonas,ageSarah,averageAgeTrue);
+
