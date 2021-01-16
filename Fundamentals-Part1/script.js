a3f268aa989f4456c0eb254bd793c5556f18d340
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-16 12:35:26 +0100

    switch statement+...

--- Fundamentals-Part1/script.js
+++ Fundamentals-Part1/script.js
@@ -1,53 +1,46 @@
-/////////////
-//Coding challenge #3
 
-/*
-there are two gymnastics team , Delphins and Koalas. They compare against
-each other 3 times . The winner with the highest average score wins the trophy!.
+const day = 'sunday';
 
-1-calculate the average score for each team using the test data below
-2- compare the team's average scores to determine the winner of the competition , 
-and print it to the console. Don't forget that there can be a draw , so test for that as well (draw
-means they have the same average score).
-
-3-Bonus 1 : Include a requirement for a minimum score of 100. with this rule , 
-a team only wins if it has a higher score of at least 100 points . HINT : uSE A LOGICAL 
-operator to test for a minimum score  as well as multiple else -if blocks.
-
-4- BONUS 2 : Minimum score also apllies to a draw! so a draw onlyhappens when both teams have 
-the same score and both  have a score greater or eual 100 points . Otherwise no teams wins the trophy.
-
-TEST DATA : Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
-TEST DATA BONUS 1 ; Dolphins score 97 , 112 and 101. Koalas score 109 , 95 and 123
-TEST DATA BONUS 2 ; Dolphins score 97 , 112 and 101. KOALS score 109 , 95 and 106
-*/
-
-//calculate the average 
-// const scoreDolphins = (96 + 108 + 89) / 3;
-// const scoreKoalas = (88+ 91 + 110)/ 3;
-// console.log(scoreDolphins , scoreKoalas);
-
-// if (scoreDolphins > scoreKoalas){
-//     console.log(`Dolphins wins the trophy (score ${scoreDolphins})`);
-// }else if (scoreKoalas > scoreDolphins){
-//     console.log(`Koalas wins the trophy (score ${scoreKoalas})`);
-
-// }else if (scoreKoalas === scoreDolphins){ // the only scenario that 's left given the previous conditions, we can just use "else"
-//     console.log("Both wins the trophy");
-// }
-
-//BONUS 1 + BONUS 2
-const scoreDolphins = (97 + 112 + 101) / 3;
-const scoreKoalas = (109 + 95 + 106)/ 3;
-console.log(scoreDolphins , scoreKoalas);
-
-if (scoreDolphins > scoreKoalas && scoreDolphins >= 100){
-    console.log(`Dolphins wins the trophy (score ${scoreDolphins})`);
-}else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100){
-    console.log(`Koalas wins the trophy (score ${scoreKoalas})`);
-
-}else if (scoreKoalas === scoreDolphins && scoreDolphins >= 100 &&  scoreKoalas >= 100 ){
-    console.log("Both wins the trophy");
-}else{
-    console.log("No one wins the trophy.");
+//without break , the code simply continuous executing .
+// the switch does a strict comparaison 
+switch(day) {
+    case 'monday' :  // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday' :
+        console.log('prepare theory videos');
+        break;
+    case 'wednesday' : // we can run the same code for two different values
+    case 'thursday' :
+        console.log("Write code examples");
+        break;
+    case 'Friday':
+        console.log('Record videos');
+        break;
+    case 'saturday' :
+    case 'sunday' :
+        console.log("Enjoy the weekend :D");
+        break;
+    // to finish we can also set a default , the dafault will be executed if all the previous failed 
+    default :
+        console.log('Not a valid day !');
 }
+
+// write the logic with different syntax .
+
+if (day === 'monday'){
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+}else if (day === 'tuesday'){
+    console.log('prepare theory videos');
+}else if (day === 'wednesday' || day === 'thursday'){
+    console.log("Write code examples");
+}else if (day === 'friday'){   
+    console.log('Record videos');
+}else if (day === 'saturday' || day === 'sunday'){
+    console.log("Enjoy the weekend :D");
+}else{
+    console.log('Not a valid day !');
+}
+
