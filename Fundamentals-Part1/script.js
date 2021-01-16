024a0cce08f1cb291ea0cde0f95bb3c4b5fc38a7
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-16 14:55:28 +0100

    coding challenge 4 + ...

--- Fundamentals-Part1/script.js
+++ Fundamentals-Part1/script.js
@@ -1,27 +1,26 @@
+//////////////////////
+//coding challenge #4
 
-const age = 15 ;
+/*
+Steven wants to build a very simple tip calculator
+for whenever he goes eating in a restaurant. In this country , it's usual to tip 15% if 
+the bill value is between 50 and 300 . if the value is different ,
+the tip is 20%.
 
-age >= 18 ? console.log("I like to drink tea") :
- console.log("I like to drink water"); // the else part 
+1- your task is to calculate the tip, depending on the bill
+value. Create a variable called 'tip' for this . It's not allowed to use an if/else statement
+.(if it's easier for you , you can start with an if /else statement , and then try to convert it to a ternary operator !)
+2-Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip).
+example : 'the bill was 275 , the tip was 41.25, and the total value 316.25'
+TEST DATA : test for bill values 275 , 40 and 430 .
 
- // the whole operator here is an expression , and the expression produce a value 
- const drink = age >= 18 ? 'wine' : 'water';
- console.log(drink);
+HINT : to calculate 20 of a value , simply multipy it by 20/100 = 0.2
+HINT : value x IS BETWEEN 50 AND 300 , if it's >= 50 && <= 300
+*/
 
- let drink2;
-
- //we need to declare the variable outside the if -else block , because
- //any variable declared inside the block is nota vailable outside.
- if (age >= 18){
-     drink2 = 'wine';
-
- }else{
-     drink2 = 'water';
- }
- console.log(drink2);
-
- // using ternary operator , we're able to transform this  if-else block of code
- // into one small operation . 
-
- //using ternary operator in template literal .
- console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+//const bill = 275;
+//const bill = 40;
+const bill = 430 ;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15  : bill * (20 / 100);
+console.log(`the bill was ${bill} , the tip was ${tip}, and the total value ${bill + tip}`);
