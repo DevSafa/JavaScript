4efbca0e08590cb013f4d5f075d8fda79a0250d5
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-17 14:12:01 +0100

    Reviewing functions+...

--- Fundamentals-Part2/script.js
+++ Fundamentals-Part2/script.js
@@ -1,14 +1,35 @@
 
-function cutFruitPieces(fruit){
-    return fruit * 4 ;
+
+/*
+const yearsUntilRetirement = (birthYear, firstName)
+=>{
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+
+    //return retirement
+    return `${firstName} retires in ${retirement} years`;
 }
-function fruitProcessor(apples, oranges){
+*/
 
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
-
-    const juice = `juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
-    return juice;
+const calcAge = function(birthYear){
+    return 2037 - birthYear;
+}
+// we will transfer it to a more normal function expression 
+// a regular function expression 
+const yearsUntilRetirement = function (birthYear, firstName){
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    if (retirement > 0)
+    {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement; // the return keyword will immediatly exit the function .
+    }
+    else
+    {
+        console.log(`${firstName} has already retired`);
+        return -1 ;
+    } 
 }
 
-console.log(fruitProcessor(2, 3));
+console.log(yearsUntilRetirement(1991 , 'Safa'));
+console.log(yearsUntilRetirement(1970 , 'Mike'));
