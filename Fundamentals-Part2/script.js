a1b45a0bce9979dfe34dc87c6e10ccff5a4d51c8
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-17 12:39:03 +0100

    Function calling other functions +...

--- Fundamentals-Part2/script.js
+++ Fundamentals-Part2/script.js
@@ -1,37 +1,14 @@
-'use strict';
 
-//function expression
-const calcAge2 = function(birthYear){
-    return 2037 - birthYear;
+function cutFruitPieces(fruit){
+    return fruit * 4 ;
+}
+function fruitProcessor(apples, oranges){
+
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
 }
 
-//Arrow function ; it's a special form of function expression ; it still a function expression actually .
-//////////////////
-
-// one parameter , one line of code when we return .
-birthYear => 2037 - birthYear;
-
-// to use the arrow function we store it in a variable:
-const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(1991);
-console.log(age3);
-
-//one parameter , more than one line of code
-const yearsUntilRetirement1 = birthYear => {
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    // write return explicitly , because we have more than one line in function definition .
-    return retirement;
-}
-
-console.log(yearsUntilRetirement1(1991));
-//more than one parameter
-const yearsUntilRetirement2 = (birthYear,firstName) => {
-    const age = 2020 - birthYear;
-    const retirement = 65 - age;
-    // write return explicitly , because we have more than one line in function definition .
-    return `${firstName} retires in ${retirement} years`;
-}
-
-console.log(yearsUntilRetirement2(1997 , 'Safa'));
-console.log(yearsUntilRetirement2(2004 , 'Inas'));
+console.log(fruitProcessor(2, 3));
