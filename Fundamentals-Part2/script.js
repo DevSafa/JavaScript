cc737fc8dde482f660a9b45faf82d8b87db37cc1
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-31 18:41:14 +0100

    Introduction to Objects+...

--- Fundamentals-Part2/script.js
+++ Fundamentals-Part2/script.js
@@ -1,38 +1,31 @@
 'use strict'
 
-/////////////////////////
-// coding challenge #2
-/*
-Steven is still building his tip calculator , using the same rules as 
-before : Tip 15% of the bill if the bill value is between 50 and 300 , 
-and if the value is different , the tip is  20% .
+// we have been using arrays as data structure to basically store 
+//multiple related values in the same variable.
+// basically it's a data structure which combines multiple values which belong 
+//to Jonas .
+//we can't reference these elements inside the array by name , but only with their order number in
+//which they appear in the array 
 
-1-write a function 'calcTip' that takes any bill value as an inpput and returns 
-the corresponding tip , calculated based on the rules above( you can check out the code 
-from first tip calculator challenge if you need to) . Use the function type you like the most.
+//defining array
+const jonasArray = [
+    'Jonas', //firstname
+    'Schmedtmann', //lastname
+    2037 - 1991, //age
+    'Teacher', //job
+    ['Michael', 'Peter', 'Steven'] //friends
+];
 
-Test the function using a bill value of 100 .
-2- and now let's use arrays! so create an array 'bills' containing the test data below.
-3-create an array 'tips' containing the tip value for each bill , calculated 
-from the function you created before.
-4-BONUS: create ann array 'total' containing the total values , so the bill + tip.
+//defining an object ==> define a new object
+//assign a name to each value, it was impossible in ararys.
 
-TEST DATA : 125, 555 and 44
-
-HINT :Remember that an array needs a value in each position , and that value can actualy
-be the returned value of a function! so you can just call a function as array values (so don't store
-the tip values in separate variables first , but right in the new array)
-*/
-
-
-const calcTip = (bill) => bill >=50 && bill <= 300 ? bill *0.15 : bill *(0.2);
-
-console.log(calcTip(100));
-
-const bills = [125 , 555 ,44];
-
-const tips = [calcTip(bills[0]) , calcTip(bills[1]) , calcTip(bills[2])];
-
-const totals = [bills[0]+tips[0] , bills[1]+tips[1] , bills[2] + tips[2]];
-
-console.log(`bills ->${bills}\ntips ->${tips}\ntotals ->${totals}`);
+//the object jonas has 5 preperties ,
+//property firstName with value of jonas
+//property lastname whith value Schmedtmann ,...
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job : 'Teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
