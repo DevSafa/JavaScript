e3d17e5fbdefc3a1e23f9830105464646d5ae9a8
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-31 16:36:06 +0100

    Basic array operations (Methods) + ...

--- Fundamentals-Part2/script.js
+++ Fundamentals-Part2/script.js
@@ -1,64 +1,65 @@
 'use strict'
 
-const friend1 = 'Michael';
-const friend2 = 'Steven';
-const friend3 = 'Peter';
-
-// litteral syntax
-const friends = ['Michael','Steven','Peter'];
-console.log(friends);
-
-//Array() is a function , new is a keyword
-const y = new Array(1991 , 1984 , 2008, 2020);
-console.log(y);
-
-//arrays are zero based
-console.log(friends[0]);
-console.log(friends[1]);
-console.log(friends[2]);
-
-//length is a property
-//number of elements of the array 
-console.log(friends.length);
-
-// the last element in the array 
-// inside square bracket js , expect an expression(produce value)
-console.log(friends[friends.length -1]);
-
-//add elements to the array
-
-friends[2] = 'Safa';
+const friends = ['Michael' , 'Steven' ,'Peter'];
 
 console.log(friends);
 
-// we can't do 
-//friends = ['Bob' , 'Alice'];
+//push method adds elements to the end of an array
+friends.push('Safa');
 
-// an array can hold values with different types all at the same time
+//push is technically  function , so it can return something .
+//the return value of push is the length of the new array .
+let newLength = friends.push('Inas');
 
-const firstName= 'safa';
-const me = [firstName, 'Barka' , 2021 - 1997 ,'1337student',friends];
-console.log(me);
-console.log(me.length);
+console.log(`array : ${friends}\nlength after push  : ${newLength}`);
 
-console.log('---------------');
-//Exercice
+//add elements to begening of the array : unshift() method
+newLength = friends.unshift('Soukaina');
+console.log(`array : ${friends}\nlength after unshift: ${newLength}`);
 
-const calcAge = function(birthYear){
-    return 2021 - birthYear;
+
+//remove elemnets : pop method
+friends.pop();
+console.log(friends);
+friends.pop();
+friends.pop();
+console.log(friends);
+
+//pop remove the last elements of the array , we don't need to pass 
+//argument to it .
+//pop method returned the removed element
+
+let popped = friends.pop();
+console.log(`removed element is : ${popped}\n array : ${friends}`);
+
+
+//remove the first element of the array 
+popped =  friends.shift();
+console.log(`removed element is : ${popped}\n array : ${friends}`);
+
+//which position a certain element is in the array  ?
+friends.push('Safa');
+console.log(friends);
+
+console.log(friends.indexOf('Safa'));
+console.log(friends.indexOf('Michael'));
+//not an element in the array
+console.log(friends.indexOf('BoB'));
+
+//ES6 method
+//instead of returning the index of the element , we simply it return true 
+//if the element is in the array , and false if it's not.
+
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Safa'));
+console.log(friends.includes('Michael'));
+
+
+friends.push(23);
+console.log(friends.includes('23')); // that's because it test with strict equality , doesn't 
+                                    // do type coercion .
+console.log(friends.includes(23));
+if (friends.includes('Safa'))
+{
+    console.log('You have a friend called Safa');
 }
-
-const years = [1990, 1967, 2002, 2010 , 2018];
-
-//NaN
-console.log(calcAge(years));
-//we cannot do operations witha arrays . years + 10(will convert the arrauy to string and add 10 to it) 
-//, years - 10(NaN);
-
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
-
-const ages = [calcAge(years[0]) , calcAge(years[1]) , calcAge(years[years.length - 1])];
-console.log(ages);
