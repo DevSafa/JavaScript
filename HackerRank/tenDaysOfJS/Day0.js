f646c3d9932d2ed1111b26702e659d650020bbe1
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-02-02 16:10:13 +0100

    Day0 (Hello, World)

---
+++ HackerRank/tenDaysOfJS/Day0.js
@@ -0,0 +1,24 @@
+//Day 0: Hello, World!
+/*
+    Sample Input 0
+        Welcome to 10 Days of JavaScript!
+    Sample Output 0
+        Hello, World!
+        Welcome to 10 Days of JavaScript!   
+        
+    greeting function 
+    It has one parameter, parameterVariable . Perform the following tasks to complete 
+    this challenge:
+    1-Use console.log() to print Hello, World! on a new line in the console, which is also
+         known as stdout or standard output.
+    Use console.log() to print the contents of parameterVariable .
+*/
+'use strict'
+
+function greeting(parameterVariable)
+{
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+}
+
+greeting('Welcome to 10 Days of JavaScript!');
