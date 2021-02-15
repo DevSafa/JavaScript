e45503ade793d521e37441fbf241073bb1c545b8
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-02-15 14:52:43 +0100

    Coding Challenge #1

--- DeveloperSkills-EditorSetup/script.js
+++ DeveloperSkills-EditorSetup/script.js
@@ -1,75 +1,45 @@
 //Remember , we're gonna use strict mode in all scripts now!
 'use strict';
 
-//PROBLEM 1
-/*we work for a company building a smart home thermometer.
-OUR MOST RECENT task is this :"Given an array of temperatures
-of one day , calculate the temperature amplitude .
-keep in mind that  sometimes there might be a sensor error ."
+///////////////////////
+//Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays 
+A String with these temperatures.
+
+example :[17,21,23]  will print
+    "...17C in 1 days...21C in 2 days...23C in 3 days..."
+create a function 'printForecast' which takes in an array 'arr'
+and logs a string like the above to the console.
+
+Use the problem-solving framework :Understand the problem and break it up into 
+sub-problemes!
+
+TEST DATA 1 : [17,21,23]
+TEST DATA 2 : [12,5,-5,0,4]
+
 */
 
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-
-//1)understanding the problem
-//what is temp amplitude?
-//how to compute max and min temperatures?
-//what's a sensor error?and what to do ?
-
-//2)Breaking up into sub-problems
-//how to ignore errors?
-//find max value in temperature array
-//find min value in temp array
-//substract min from max (amplitude) and return it .
-
-//SOLUTION
-//temperature amplitude is the difference between the highest
-//and lowest temperature in the array
-
-const calcTempAmplitude = function (temps) {
-  // find max  and min
-  let max = temps[0];
-  let min = temps[0];
-
-  for (let i = 1; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-    if (temps[i] != 'error' && temps[i] > max) max = temps[i];
-    if (temps[i] != 'error' && temps[i] < min) min = temps[i];
-  }
-  //console.log(max, min);
-  return max - min;
-};
-calcTempAmplitude([3, 7, 4, 1, 8]);
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
-//console.log(`amplitude is : ${calcTempAmplitude(temperatures)}`);
-
-//PROBLEM 2
-//1)understanding the problem
-//with 2 arrays , should we implement functionality twice ?
-//    NO! just merge two arrays
-
-//2)Breaking up into sub-problems
-// - How to merge 2 arrays?
-
 //SOLUTION
 
-const calcTempAmplitudeNew = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
-  // find max  and min
-  let max = temps[0];
-  let min = temps[0];
+//1)understanding the problem
+// -Arary transformed to string ,separated by ...
+// -What is the X days? Answer :index + 1
 
-  for (let i = 1; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-    if (temps[i] != 'error' && temps[i] > max) max = temps[i];
-    if (temps[i] != 'error' && temps[i] < min) min = temps[i];
-  }
-  //console.log(max, min);
-  return max - min;
+//2)Breaking up into sub-problems
+// - transform array into string
+// - transform each element to string with °C
+// - string to contain day (index + 1)
+// - add ...  between elements and start and end of string
+// - Log string to console
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) str += `${arr[i]}°C in ${i + 1} days ... `;
+  console.log('...' + str);
 };
 
-const amplitudeNew = calcTempAmplitudeNew([3,5,1] ,[9,0,5]);
-console.log(amplitudeNew);
+const arr1 = [17, 21, 23];
+const arr2 = [12, 5, -5, 0, 4];
+printForecast(arr1);
+printForecast(arr2);
