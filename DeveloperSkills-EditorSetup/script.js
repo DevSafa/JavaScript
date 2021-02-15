ff084e2df38e16ba63153e5764c101eb07713498
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-02-15 10:47:17 +0100

    Setting up Prettier and VS Code

---
+++ DeveloperSkills-EditorSetup/script.js
@@ -0,0 +1,14 @@
+//Remember , we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = 23;
+
+if (xyz === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+//configure snippets in vs code for console.log
+//we can type just cl instead of console.log
+console.log(xyz);
+
+
