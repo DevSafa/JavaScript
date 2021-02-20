8a362c91f40fa2ab0f990b9702121a522c35f9e2
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-02-20 10:32:25 +0100

    Find A bug and fix it using functions of console object+...

--- DeveloperSkills-EditorSetup/script.js
+++ DeveloperSkills-EditorSetup/script.js
@@ -1,45 +1,31 @@
 //Remember , we're gonna use strict mode in all scripts now!
 'use strict';
 
-///////////////////////
-//Coding Challenge #1
+// using the console to find a BUG and fix it 
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celius',
+    //the value of measurement will come from a prompt
+    //value: prompt('Degrees celsius:'),
+    //C)//FIX THE BUG
+    value: Number(prompt('Degrees celsius:')),
+  };
 
-/*
-Given an array of forecasted maximum temperatures, the thermometer displays 
-A String with these temperatures.
+  //the prompt always return a string
+  console.log(measurement);
 
-example :[17,21,23]  will print
-    "...17C in 1 days...21C in 2 days...23C in 3 days..."
-create a function 'printForecast' which takes in an array 'arr'
-and logs a string like the above to the console.
-
-Use the problem-solving framework :Understand the problem and break it up into 
-sub-problemes!
-
-TEST DATA 1 : [17,21,23]
-TEST DATA 2 : [12,5,-5,0,4]
-
-*/
-
-//SOLUTION
-
-//1)understanding the problem
-// -Arary transformed to string ,separated by ...
-// -What is the X days? Answer :index + 1
-
-//2)Breaking up into sub-problems
-// - transform array into string
-// - transform each element to string with °C
-// - string to contain day (index + 1)
-// - add ...  between elements and start and end of string
-// - Log string to console
-const printForecast = function (arr) {
-  let str = '';
-  for (let i = 0; i < arr.length; i++) str += `${arr[i]}°C in ${i + 1} days ... `;
-  console.log('...' + str);
+  //B)Find the BUG
+  //another way of showing objects in the console
+  console.table(measurement);
+  //console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  //coonvert from celsius to kelvin by adding 273
+  const Kelvin = measurement.value + 273;
+  return Kelvin;
 };
+//A)Identify the BUG
+console.log(measureKelvin());
 
-const arr1 = [17, 21, 23];
-const arr2 = [12, 5, -5, 0, 4];
-printForecast(arr1);
-printForecast(arr2);
+
