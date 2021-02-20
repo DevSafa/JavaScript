2baa1a8d3fd2d94c0738d06841adb88335fa679c
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-02-20 10:50:48 +0100

    using the teh chrome debugger to fix a abug+...

--- DeveloperSkills-EditorSetup/script.js
+++ DeveloperSkills-EditorSetup/script.js
@@ -1,31 +1,33 @@
 //Remember , we're gonna use strict mode in all scripts now!
 'use strict';
 
-// using the console to find a BUG and fix it 
-const measureKelvin = function () {
-  const measurement = {
-    type: 'temp',
-    unit: 'celius',
-    //the value of measurement will come from a prompt
-    //value: prompt('Degrees celsius:'),
-    //C)//FIX THE BUG
-    value: Number(prompt('Degrees celsius:')),
-  };
+//Find a Bug using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
 
-  //the prompt always return a string
-  console.log(measurement);
 
-  //B)Find the BUG
-  //another way of showing objects in the console
-  console.table(measurement);
-  //console.log(measurement.value);
-  // console.warn(measurement.value);
-  // console.error(measurement.value);
-  //coonvert from celsius to kelvin by adding 273
-  const Kelvin = measurement.value + 273;
-  return Kelvin;
+  // find the bug using the debugger
+  // let max = 0;
+  // let min = 0;
+  
+  //fix the bug 
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 1; i < temps.length; i++) {
+
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    //js had a ddebugger statement , so when the browser see debugger;
+    //it will automatically open the debugger tool .
+    //debugger;
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  console.log(max , min);
+  return max - min;
 };
-//A)Identify the BUG
-console.log(measureKelvin());
-
-
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+//A)Identify the Bug
+console.log(amplitudeBug);
