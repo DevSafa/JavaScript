22efaed6cbb6a6968677a4b791a68b86ba0de7e6
sbarka-safa <sbarka@e1r12p12.1337.ma>
2021-03-06 12:00:30 +0100

    Refactoring our code

--- DOM-EventFundamentals/script.js
+++ DOM-EventFundamentals/script.js
@@ -1,20 +1,24 @@
 'use strict';
 
-
+//refactoring basically means restruct the code but without changing how it works
+//sometimes a good refactoring technique is by creating functions
 let  secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 //store the high score in a variable
 let hightscore = 0;
 
-
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
 document.querySelector('.check').addEventListener('click' , function(){
     const guess = Number(document.querySelector('.guess').value);
    
     if (!guess){
-        document.querySelector('.message').textContent = 'No number!';
+        displayMessage('No number!');
 
+    //when player wins
     }else if (guess === secretNumber ){ 
-        document.querySelector('.message').textContent = 'Correct Number!';  
+        displayMessage('Correct Number!');  
         document.querySelector('.number').textContent = secretNumber;
    
         document.querySelector('body') .style.
@@ -26,37 +30,24 @@ document.querySelector('.check').addEventListener('click' , function(){
             document.querySelector('.highscore').textContent = hightscore;
         }
         
-
-    } else if (guess > secretNumber){
+    //when guess is wrong
+    }else if (guess !== secretNumber){
         if (score > 1){
-            document.querySelector('.message').textContent = 'Too high!';  
+            displayMessage( guess > secretNumber ? 'Too high!' : 'Too Low!');  
             score--;
             document.querySelector('.score').textContent = score;
         }else{
-            document.querySelector('.message').textContent = 'You lost the game!';
+            displayMessage('You lost the game!');
             document.querySelector('.score').textContent = 0;
         }
-    
-    }else if (guess < secretNumber){
-        if (score > 1){
-            document.querySelector('.message').textContent = 'Too Low!';  
-            score--;
-            document.querySelector('.score').textContent = score;
-        }else{
-            document.querySelector('.message').textContent = 'You lost the game!';
-            document.querySelector('.score').textContent = 0;
-        }
-        
-    }
-  
-
+    } 
 });
 
 document.querySelector('.again').addEventListener('click',function(){
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-    document.querySelector('.message').textContent = 'Start guessing...'; 
+    displayMessage('Start guessing...'); 
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent ='?';
     document.querySelector('.guess').value ='';
