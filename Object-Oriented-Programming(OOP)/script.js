6d565349ca41953458f1cd5382f2a59a665b1cfe
sbarka-safa <sbarka@e1r11p2.1337.ma>
2021-04-01 10:09:56 +0100

    Prototypes

--- Object-Oriented-Programming(OOP)/script.js
+++ Object-Oriented-Programming(OOP)/script.js
@@ -1,83 +1,102 @@
 'use strict';
 
-//constructor functions
-//we can use constructor functions to build an object using a function
-// a constructor function is actually a completely normal function 
-// the only difference between a regular function and a constructor function is that we call a constructor function
-// with new operator.
-
-//create a constructor function for a person , constructor functions always start with a capital letter (convention)
-// in fact  other built-in constructors like Array or map follow that convention
-
-// an arrow function will actually not work as a  function constructor  , that's because it doesn't have it's own this keyword and we need that
-//only function declarations , and function expression that works
-
-//this function will produce an object
+//Prototypes
 const Person = function(firstName, birthYear){
-    //because we called the function  with new keyword , this refer to the empty object created
-    //the this keyword should be the empty object created in step 1 
-    // console.log(this);
-    // create a property on this keyword 
-    // the instance properties , these properties will available on all instances created
-    // through this constructor function.
+  
     this.firstName = firstName;
     this.birthYear = birthYear;
 
-
-    // we can also add methods to that object
-    // you should never create a method inside the constructor function .
-    // it's a really bad practice  
-    
-
-    //why?
-    /*
-        that's because imagine that we're gonna create a hundred or thousands of Person
-        object using this constructor function . so each of these objects would carry arround this function.
-        if we had thousand objects , we would essentially ctreate a thousand copy of this function
-        and that will be terrible for the code performance , so don't do this.
-        so to solve this probleme we will use protoypes and prototype inheritance.
-    */
-    this.calcAge = function(){
-        console.log(2037 - this.birthYear);
-    };
+    // this.calcAge = function(){
+    //     console.log(2037 - this.birthYear);
+    // };
 }
-
-// call the constructor new keyword
-// the new operator is a special operator  because what it does , is to call the Person function
 
 const jonas = new Person('Jonas', 1991);
 console.log(jonas);
-
-//when calling a function constructor with new keyword . what happens behind the scenes?
-/*
-    1- a new empty object is created {}
-    2- function is called , and this keyword will be set to the newly created object this = {}(this = the new empty object)
-        in the execution context of the function , the this keyword  will point to the the empty object created
-    3- the newly created object {} is linked to a prototype
-    4- object that was created in the begening  is then automatically returned from the constructor function 
-*/
-
-// we can use the constructor function to create many object as we want
-//we can create many object based on this constructor function
 const matilda = new Person('Matilda',2017);
 const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
-// in classical object oriented programming :
-// an object created from a class is called an instance,
-// now , we didn't techniclly create a class here  because js doesn't really have classes in the sense of traditionnel oop
-// however we did create an object from the constructor function , 3 objects.
-// constructor function have been used since the begening of js to kind of simulate classes
-// therefore we cam say that jonas is an instance of Person.
-// there is even an opertaor to test that :
 console.log(jonas instanceof Person);
 
 const jay = 'jay';
 
 console.log(jay instanceof Person);
 
+//Prototypes
 /**
- * constructor functions are not really a feature of javascript language.
- * instead there are simply a pattern that has been developped by other developpers. 
- * the new magic here is the new operator 
+ * each and every function in javascript automatically has  a property called prototype
+ * and that includes of course constructor functions, every object created by a certain  constructor function
+ * will get access to all the methods and properties that we define on the constructors prototype property:
+ *   Person.prototype (prototype is a property of the constructor function)
+ * 
+ * all the objects created throught the Person constructor function  will inherit , so they will get  access to all the methods and properties 
+ * that are defined on this prototype property
  */
+console.log(Person.prototype);
+
+//let add a method to the prototype property
+// so it will exist only one copy of this functions.
+// all the objects craeted using Person constructor function can basically reuse this function on themselfs
+//this keyword is always set to the object calling the method.
+//calcAge is located in the prototype property of Person.
+Person.prototype.calcAge = function (){
+    console.log(2037 - this.birthYear);
+};
+
+//each object created to the constructor function will get access to the  methods of this prototype property , and so of course also to calcAge
+
+//we can use the method calcAge on the jonas Object even if it's not in the object itself.
+//the jonas object had the birthYear and firstName , but it does not contain the calcAge method,
+//but still we can access to it because of prototype inheritance
+
+
+jonas.calcAge();
+matilda.calcAge();
+
+//why does it actually worls?
+/*it works because NY OBJECT ALWAYS  has access to the methods and properties for it's prototype.
+the prototype of jonas and matilda is Person.prototype
+*/
+//the prototype of jonas
+//the prototype of jonas object is essentially the prototype property of the constructor function
+console.log(jonas.__proto__);
+console.log(Person.prototype);
+console.log(jonas.__proto__ === Person.prototype);
+
+//that confirms that Person.prototype is indeed the prototype of jonas
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+
+
+//thsi very common confusion comes from bad naming of this Person property
+//prototype here is prototype of linked objects not prototype of Person
+//probably shouldn't be called prototype but instead something like .prototypeOfLinkedObject
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+//where the __proto property on jonas comes from?
+/*
+remember the new operator , step 3 
+3.{} linked to prototype
+-the step number 3 that creates the __proto__ property .
+it creates the __proto__ property on the object and sets it's value to the prototype property of the function that's being
+called.(the constructor functions).
+so this is how javascript knows internally the the jonas object  is connected to the Person.prototype
+
+*/
+
+// we can also set properties on prototype not just methods
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas,matilda);
+//both the objects jonas and matilda will inherits , so they will access to the species property from the prototype,
+//when we access the object in the console , the property is not directly in the object , so it's not it's 
+//own property .own properties are only the one that are declared directly on the object itself, not including
+//the inherited properties . 
+console.log(jonas.species, matilda.species);
+
+//in js there is a  way of checking of the own properties
+console.log(jonas.hasOwnProperty('firstName'));
+//species property is not really inside the jonas object , it's simply has access to it because of it's prototype, the prototype 
+//property of Person
+console.log(jonas.hasOwnProperty('species'));
+
