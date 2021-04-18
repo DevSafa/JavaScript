f3ce2538b0cbce781b0855d4107b3a3a714a20a8
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-04-18 16:21:06 +0000

    Object.create

--- Object-Oriented-Programming(OOP)/script.js
+++ Object-Oriented-Programming(OOP)/script.js
@@ -1,60 +1,63 @@
 'use strict';
 
 
-//static methods
-/*
-example:
-*/
-console.log(Array.from(document.querySelector('h1')));
-console.log(Array.from(document.querySelectorAll('h1')));
-//the from method here is really a method that is atatched to the Array constructor
-//so we could not use the from() method on an array 
-    //console.log([1,2,3].from()) //-->it will not work.
-//the from() method is really atatched to the entire constructor , the Array constructor and not to the 
-//prototype property of the constructor, therefore , all the arrays do not inherit this method from(), because it's not on their prototype
-//it simply atatched to the constructor itself
-//we can also say that from() method is on the Array namepace
+//we learned about constructor functions and ES6 classes ,
+//but actually there is a third way of implementing prototypal inheritance or delegation as we can also call it 
 
+//that third way is to use a function called Object.create which works in different way than constructor functions  and classes
 
-//constructor function and new keyword
-const Person= function(firstName, birthYear)
-{
-    this.firstName= firstName;
-    this.birthYear= birthYear;
-};
+//with Object.create they still the idea of prototypal inheritance , however , 
+//there are no prototype properties involved  and also no constructor functions , and no new operator.
+//. so instead we can use Object.create to essentially manually set the prototype of an object to any other
+//object that we want.
 
-//creat jonas object
-const jonas =new Person('jonas',1991);
-console.log(jonas);
+//if we can set the prototype to any object , let's actualy create an object , that 
+//we want to be the prototype of all the person objects.
 
+//this object will be the literaly the prototype of all the persons object
+const PersonProto = {
+    //that's the method that we want the Person object to inherit
+    calcAge(){
+        console.log(2037 - this.birthYear);
+    },
 
-// add a static method to the constructor function
-Person.hey = function(){
-    console.log('Hey there .');
-    //whanever the object calling the method will be the this keyword inside the method
-    console.log(this);
-};
-
-Person.hey();
-
-//the hey() method is not inherited
-//the hey() method is not in the prototype of jonas object , so there is no way that jonas object could inherit it 
-//we cannot say jonas.hey();
-
-//class declaration
-
-class PersonCl {
-    constructor(fullName, birthYear) {
-        this.fullName = fullName;
-        this.birthYear = birthYear;
+    init(firstName, birthYear){
+        this.firstName= firstName;
+        this.birthYear=birthYear;
     }
-
-   //create static method
-   //the static methods are not available on the instances
-   static hey(){
-       console.log('Hey from Class');
-       
-   } 
 }
 
-PersonCl.hey();
+//create a person object wth the PersonProto as prototype
+// it will return a new object  that is linked to the prototype PersonProto
+// steven is an empty object , and it will be linked to this PersonProto object which will be it's 
+//prototype.
+//implement prototypal inheritance but in differnt way
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+//when we use the new operator in constructor functions or classes it automatiically
+//sets the prototype of instances to the constructor prototype property . this happens automatically.
+
+//on the other hand with Object.create , we can set the protoype of object  manually to any object
+//that we want. in our case , we manyually set the prototype of steven object to the PersonProto object
+//now the two objects are linked throught the proto property just like before.
+
+//Object.create in real world is the least used way for implementing prototypal inheritance
+
+// we will nedd Object.CREATE to link prototypes in next lecture in order to implement inheritance between classes
+
+//it will be exactly the object that we specify inside Object.create() METHOD : PersonProto
+console.log(steven.__proto__);
+console.log(steven.__proto__===PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah',1979);
+sarah.calcAge();
+
+console.log(sarah);
+console.log(steven);
+
+//Object.create() create a new object and the prototype of that object is the object that we pass to create method
