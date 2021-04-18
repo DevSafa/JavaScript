647135c503a4988c30670036e49559b91d449449
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-04-18 14:53:06 +0000

    Static Methods

--- Object-Oriented-Programming(OOP)/script.js
+++ Object-Oriented-Programming(OOP)/script.js
@@ -17,15 +17,44 @@ console.log(Array.from(document.querySelectorAll('h1')));
 
 
 //constructor function and new keyword
-const Person= function(firstName, birthName)
+const Person= function(firstName, birthYear)
 {
     this.firstName= firstName;
     this.birthYear= birthYear;
 };
 
+//creat jonas object
+const jonas =new Person('jonas',1991);
+console.log(jonas);
+
+
 // add a static method to the constructor function
-Person.hey =function(){
+Person.hey = function(){
     console.log('Hey there .');
+    //whanever the object calling the method will be the this keyword inside the method
+    console.log(this);
 };
 
 Person.hey();
+
+//the hey() method is not inherited
+//the hey() method is not in the prototype of jonas object , so there is no way that jonas object could inherit it 
+//we cannot say jonas.hey();
+
+//class declaration
+
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+   //create static method
+   //the static methods are not available on the instances
+   static hey(){
+       console.log('Hey from Class');
+       
+   } 
+}
+
+PersonCl.hey();
