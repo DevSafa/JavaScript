665ae4cacceccdc03f02f9ac2925bec35cdeca33
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-04-18 16:50:41 +0000

    Coding Challeng #2

--- Object-Oriented-Programming(OOP)/script.js
+++ Object-Oriented-Programming(OOP)/script.js
@@ -1,63 +1,56 @@
 'use strict';
 
 
-//we learned about constructor functions and ES6 classes ,
-//but actually there is a third way of implementing prototypal inheritance or delegation as we can also call it 
+////////////////////////////
+//Coding challenge #2
 
-//that third way is to use a function called Object.create which works in different way than constructor functions  and classes
+/*
+1-Re-create challenge1 , but this time using an ES6 class;
+2-Add a getter called 'speedUS' which returns the current speed in mi/h
+(divided by 1.6);
 
-//with Object.create they still the idea of prototypal inheritance , however , 
-//there are no prototype properties involved  and also no constructor functions , and no new operator.
-//. so instead we can use Object.create to essentially manually set the prototype of an object to any other
-//object that we want.
+3-Add a setter called 'speedUS' which sets the current speed in mi/h (but convert it to 
+km/h before storing the value, by multiplying the input by 1.6);
+4-Create a new car and experiment with the accelerate  and brake methods , and with the getter and setter
 
-//if we can set the prototype to any object , let's actualy create an object , that 
-//we want to be the prototype of all the person objects.
+DATA CAR1 : 'Ford' going at 120 km/h
 
-//this object will be the literaly the prototype of all the persons object
-const PersonProto = {
-    //that's the method that we want the Person object to inherit
-    calcAge(){
-        console.log(2037 - this.birthYear);
-    },
+Good Luck
+*/
 
-    init(firstName, birthYear){
-        this.firstName= firstName;
-        this.birthYear=birthYear;
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed= speed;
+    }
+    //the original speed is in km/h => we want to conver it to mies/h so we devide by 1.6
+   
+
+    accelerate(){ //there is a difference between that and accelerate = function(){} ...
+        this.speed +=10;
+        console.log(`${this.make} is going at ${this.speed}km/h`);
+    }
+
+    brake()
+    {
+        this.speed -=5;
+        console.log(`${this.make} is going at ${this.speed}km/h`);
+    }
+    //transform the method speedUS  to a property using get keyword
+    get speedUS() {
+        return this.speed / 1.6;
+    }
+
+    set speedUS(speed){
+        this.speed = speed * 1.6;
     }
 }
 
-//create a person object wth the PersonProto as prototype
-// it will return a new object  that is linked to the prototype PersonProto
-// steven is an empty object , and it will be linked to this PersonProto object which will be it's 
-//prototype.
-//implement prototypal inheritance but in differnt way
-const steven = Object.create(PersonProto);
-console.log(steven);
-steven.name = 'Steven';
-steven.birthYear = 2002;
-steven.calcAge();
-
-//when we use the new operator in constructor functions or classes it automatiically
-//sets the prototype of instances to the constructor prototype property . this happens automatically.
-
-//on the other hand with Object.create , we can set the protoype of object  manually to any object
-//that we want. in our case , we manyually set the prototype of steven object to the PersonProto object
-//now the two objects are linked throught the proto property just like before.
-
-//Object.create in real world is the least used way for implementing prototypal inheritance
-
-// we will nedd Object.CREATE to link prototypes in next lecture in order to implement inheritance between classes
-
-//it will be exactly the object that we specify inside Object.create() METHOD : PersonProto
-console.log(steven.__proto__);
-console.log(steven.__proto__===PersonProto);
-
-const sarah = Object.create(PersonProto);
-sarah.init('Sarah',1979);
-sarah.calcAge();
-
-console.log(sarah);
-console.log(steven);
-
-//Object.create() create a new object and the prototype of that object is the object that we pass to create method
+const ford = new Car('Ford', 120);
+console.log(ford.__proto__);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50 //(mi/h)
+console.log(ford);
